9c979ef47bb5dbcd12490c9c590c4a907c109b0a
DarkEden-coding <darkedenc9@gmail.com>
2023-05-02 21:10:55 -0400

    sad

---
+++ main.py
@@ -0,0 +1,64 @@
+# move 2 stepper motors
+
+import time
+import RPi.GPIO as GPIO
+
+# GPIO pins for the 2 motors
+motor1_pins = (11, 12, 13, 15)
+motor2_pins = (16, 18, 22, 7)
+
+# GPIO setup
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(motor1_pins, GPIO.OUT)
+GPIO.setup(motor2_pins, GPIO.OUT)
+
+
+# motor1
+def motor1(direction, steps):
+    step = 0
+    while step < steps:
+        for pin in range(0, 4):
+            GPIO.output(motor1_pins[pin], direction[step][pin])
+        step += 1
+        time.sleep(0.001)
+
+
+# motor2
+def motor2(direction, steps):
+    step = 0
+    while step < steps:
+        for pin in range(0, 4):
+            GPIO.output(motor2_pins[pin], direction[step][pin])
+        step += 1
+        time.sleep(0.001)
+
+
+# clockwise
+clockwise = [
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [0, 0, 0, 1],
+    [1, 0, 0, 1],
+]
+
+# counterclockwise
+counter_clockwise = [
+    [1, 0, 0, 1],
+    [0, 0, 0, 1],
+    [0, 0, 1, 1],
+    [0, 0, 1, 0],
+    [0, 1, 1, 0],
+    [0, 1, 0, 0],
+    [1, 1, 0, 0],
+    [1, 0, 0, 0],
+]
+
+# move motor1 clockwise 2048 steps
+motor1(clockwise, 2048)
+
+# move motor2 counterclockwise 2048 steps
+motor2(counter_clockwise, 2048)
